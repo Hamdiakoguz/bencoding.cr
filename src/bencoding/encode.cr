92cb795470068df5d683fe2b0098d7e6f6453e02
Hamdi Akoğuz <hamdiakoguz@gmail.com>
2016-07-11 21:04:09 +0300

    Add encoder

--- src/bencoding/encode.cr
+++ src/bencoding/encode.cr
@@ -1,4 +1,39 @@
 module BEncoding
   def self.encode(param)
+    io = MemoryIO.new
+    Encoder.new(io).encode(param)
+    io.to_s
+  end
+
+  class Encoder
+    def initialize(@io : IO)
+    end
+
+    def encode(param : Int)
+      @io << NUMBER_START
+      @io << param.to_s
+      @io << NUMBER_END
+    end
+
+    def encode(param : String)
+      @io << param.size
+      @io << BYTE_ARRAY_DIVIDER
+      @io << param
+    end
+
+    def encode(param : Hash(String, T))
+      @io << DICTIONARY_START
+      param.keys.sort.each do |key|
+        encode(key)
+        encode(param[key])
+      end
+      @io << DICTIONARY_END
+    end
+
+    def encode(param : Enumerable)
+      @io << LIST_START
+      param.each {|p| encode(p) }
+      @io << LIST_END
+    end
   end
 end
